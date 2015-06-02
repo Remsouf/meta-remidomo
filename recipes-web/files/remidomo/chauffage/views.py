259ba7a2cfd4176f06fd63967820c5df30c76f84
Remi Peuvergne <remi.peuvergne@openwide.fr>
2015-06-02 17:19:29 +0200

    Afficher les vraies infos dans la vue statut, en lisant la DB

--- recipes-web/files/remidomo/chauffage/views.py
+++ recipes-web/files/remidomo/chauffage/views.py
@@ -1,10 +1,39 @@
+import datetime
 from django.shortcuts import render
+from models import Mesure
+
+# TODO: Read those from config... (but when ?)
+SENSORS = ('terrasse', 'salon')
+
+def __elapsed_time(timestamp):
+    delta = datetime.datetime.now() - timestamp.replace(tzinfo=None)
+
+    if delta.total_seconds() < 30:
+        return "A l'instant"
+    elif delta.total_seconds() < 60:
+        return 'Il y a %ds' % delta.total_seconds()
+    elif delta.total_seconds() < 60*60:
+        return 'Il y a %dmin' % (delta.total_seconds() / 60)
+    elif delta.total_seconds() < 24*60*60:
+        return 'Il y a %dh' % (delta.total_seconds() / (60*60))
+    else:
+        return 'Il y a %dj' % delta.days
 
 def status(request):
-    context = {'temperature_ext': 45.6,
-               'ilya_ext': '25 s',
-               'temperature_int': 22.2,
-               'ilya_int': '25 min' }
+    sensor_data = []
+    for sensor_name in SENSORS:
+        rows = Mesure.objects.filter(name=sensor_name).order_by('-timestamp')
+        if rows is None:
+            current_temp = '?'
+            since_when = ''
+        else:
+            current_temp = rows[0].value
+            since_when = __elapsed_time(rows[0].timestamp)
+        sensor_data.append({'name': sensor_name.capitalize(),
+                            'temp': current_temp,
+                            'since_when': since_when})
+
+    context = {'data': sensor_data}
     return render(request, 'status.html', context)
 
 def programmation(request):
