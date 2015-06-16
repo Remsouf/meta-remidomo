6afeb69ad37f56bb52817713167e3fe05e3f90c9
Remi Peuvergne <rpeuvergne@free.fr>
2015-06-16 21:10:16 +0200

    Ajout statut puissance electrique (vue statut)

--- recipes-web/files/remidomo/views.py
+++ recipes-web/files/remidomo/views.py
@@ -110,24 +110,39 @@ def logs(request):
 def status(request):
     config = __get_config()
     if config is not None:
-        names = config.get_sensor_names()
+        names = config.get_temp_sensor_names()
     else:
         names = list()
 
-    sensor_data = []
+    # Temperature sensors
+    temp_data = []
     for sensor_name in names:
-        rows = Mesure.objects.filter(name=sensor_name).order_by('-timestamp')
+        rows = Mesure.objects.filter(name=sensor_name).order_by('-timestamp')[:1]
         if rows is None or len(rows) == 0:
-            current_temp = '?'
-            since_when = ''
+            current_temp = ''
+            since_when = '?'
         else:
             current_temp = rows[0].value
             since_when = __elapsed_time(rows[0].timestamp)
-        sensor_data.append({'name': sensor_name.capitalize(),
-                            'temp': current_temp,
-                            'since_when': since_when})
+        temp_data.append({'name': sensor_name.capitalize(),
+                          'temp': current_temp,
+                          'since_when': since_when})
 
-    context = {'data': sensor_data}
+    # Power sensor
+    power_name = config.get_power_sensors().keys()[0]
+    rows = Mesure.objects.filter(name=power_name).order_by('-timestamp')[:1]
+    if rows is None or len(rows) == 0:
+        current_power = ''
+        since_when = '?'
+    else:
+        current_power = rows[0].value
+        since_when = __elapsed_time(rows[0].timestamp)
+
+    power_data = { 'name': power_name.capitalize(),
+                   'power': current_power,
+                   'since_when': since_when }
+    context = {'temperature': temp_data,
+               'power': power_data}
     return render(request, 'status.html', context)
 
 def program(request):
@@ -191,19 +206,28 @@ def graph(request, dataset_name):
     local_offset = local_tz.utcoffset(datetime.datetime.now(local_tz))
     local_offset_hours = local_offset.total_seconds() / 3600
 
+    config = __get_config()
+
     if dataset_name == 'all':
-        config = __get_config()
         if config is not None:
-            names = config.get_sensor_names()
+            names = config.get_temp_sensor_names()
         else:
             names = list()
+
+        units = '\u00B0C'
     else:
         names = list()
         names.append(dataset_name)
 
+        if config is None or dataset_name in config.get_temp_sensor_names():
+            units = '\u00B0C'
+        else:
+            units = 'W'
+
     context = { 'names': names,
                 'dataset_name': dataset_name,
-                'time_offset': local_offset_hours }
+                'time_offset': local_offset_hours,
+                'units': units }
     return render(request, 'graph.html', context)
 
 def config(request):
@@ -278,15 +302,15 @@ def config_post(request):
 Return graph data in JSON format
 """
 def data(request, name):
-    config = __get_config()
-    if config is not None:
-        names = config.get_sensor_names()
-    else:
-        names = list()
-
     js_cols = [{'label':'dates', 'type':'datetime'}]
 
     if name == 'all':
+        config = __get_config()
+        if config is not None:
+            names = config.get_temp_sensor_names()
+        else:
+            names = list()
+
         rows = Mesure.objects.order_by('timestamp')
         for sensor_name in names:
             js_cols.append({'label': sensor_name.capitalize(), 'type': 'number'})
